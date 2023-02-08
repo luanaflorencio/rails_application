58c7d3e559dd5d4293b224a2a094d1c669950912
luanaflorencio-viitra <luana.florencio@viitra.com>
2023-02-08 14:33:48 -0300

    Testando variável de sessão em arquivos conectados

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def date_br(date_us)
         date_us.strftime("%d/%m/%Y")
     end
+
+    def environment_rails
+        if Rails.env.production?
+            "Produção"
+        elsif Rails.env.development?
+            "Desenvolvimento"
+        else
+            "Teste"
+        end 
+    end
 end
